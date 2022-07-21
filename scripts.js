fd81ac7f1003e01a523e755e7b268a5a95dfc569
James Gonzalez <jamesg8025@gmail.com>
2022-07-21 18:30:07 -0500

    Apply flexbox to content

--- scripts.js
+++ scripts.js
@@ -7,10 +7,10 @@ function makeRows(rows, cols) {
     // create for loop to determine size of grid
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
-        cell.innerText = (c + 1);
+        // cell.innerText = (c + 1);
         container.appendChild(cell).className = 'grid-item';
     };
 
 };
 
-makeRows(16, 16);
+makeRows(100, 100);
