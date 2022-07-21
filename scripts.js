41dfa7fb7ce0f30d3c534cc3f62a79725a691223
James Gonzalez <jamesg8025@gmail.com>
2022-07-21 16:54:58 -0500

    Create function to generate new cells in grid

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.grid-container');
+
+function makeRows(rows, cols) {
+    // first rename rows and columns
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    // create for loop to determine size of grid
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = 'grid-item';
+    };
+
+};
